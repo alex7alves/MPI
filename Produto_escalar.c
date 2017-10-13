90d6e3cde80c23d575fb048bf0f005ca6fc35872
alex7alves <alex7alves10@hotmail.com>
2017-10-13 09:34:27 -0300

    Resultado do produto escalar pronto

--- Produto_escalar.c
+++ Produto_escalar.c
@@ -15,15 +15,15 @@
 void inicio(int* tamanho,int* local_n,int total,int rank,int* escalar);
 void iniciar_vetores(int* tamanho,int* local_n,int rank
       ,double *vetor1,double *vetor2,double *vetor_local1,double *vetor_local2); 
-void Soma_locais(int* local_n,double *vetor_local1,int *escalar, double *vetor_local2,double *soma_local);
+void Soma_locais(int* local_n,double *vetor_local1,int *escalar, double *vetor_local2,double *soma_local,double *resultado,int rank);
 int main(void) {
    int rank , total;
    MPI_Status stat;
    int *tamanho; // tamanho do vetor
    int *local_n;  // tamanho do vetor dividido
-   int *escalar,*resultado;
+   int *escalar,i;
    double *vetor1,*vetor2, *soma_local;
-   double *vetor_local1,*vetor_local2;
+   double *vetor_local1,*vetor_local2,*resultado;
    // alocando memoria
    tamanho= malloc(sizeof(int));
    escalar= malloc(sizeof(int));
@@ -33,8 +33,33 @@ int main(void) {
    MPI_Comm_size(MPI_COMM_WORLD, &total); 
    MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
       inicio(tamanho,local_n,total,rank,escalar);
+      vetor1 = malloc(*tamanho * (sizeof(double)));
+   	  vetor2 = malloc(*tamanho * (sizeof(double)));
+   	  vetor_local1 = malloc(*local_n*sizeof(double));
+   	  vetor_local2 = malloc(*local_n*sizeof(double));
       iniciar_vetores(tamanho,local_n,rank,vetor1,vetor2,vetor_local1,vetor_local2);
-      Soma_locais(local_n,vetor_local1,escalar,vetor_local2,soma_local);
+     // Soma_locais(local_n,vetor_local1,escalar,vetor_local2,soma_local,resultado,rank);
+   		
+   	  soma_local = malloc(sizeof(double));
+      resultado= malloc(sizeof(double));
+   // calculando as somas locais
+      for(i=0;i<*local_n;i++){
+	      vetor_local1[i] = vetor_local1[i] *(*escalar);
+	      vetor_local1[i] =vetor_local1[i]*vetor_local2[i];
+	      *soma_local = (*soma_local) + vetor_local1[i];
+	   
+	   }
+   		//Adiciona as somas locais no resultado
+   		MPI_Reduce(soma_local, resultado, 1, MPI_DOUBLE, MPI_SUM, 0,
+       		 MPI_COMM_WORLD);
+
+	   if(rank == 0){
+	      printf("O resultado final: %lf\n",*resultado);
+	      free(vetor_local1);
+	      free(vetor_local2);
+	    }
+
+
    MPI_Finalize();
     
   
@@ -50,55 +75,79 @@ void inicio(int* tamanho,int* local_n,int total,int rank,int* escalar){
          printf(" Entre com o valor do escalar\n");
          scanf("%d",escalar);
          *local_n = *tamanho/total;
+         } 
          // Envia local_n para todos os processos
           MPI_Bcast(local_n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-      } 
+          MPI_Bcast(tamanho, 1, MPI_INT, 0, MPI_COMM_WORLD);
+          MPI_Bcast(escalar, 1, MPI_INT, 0, MPI_COMM_WORLD);
+      
+      //*local_n = *tamanho/total;
       
 }
 
 void iniciar_vetores(int* tamanho,int* local_n,int rank
       ,double *vetor1,double *vetor2,double *vetor_local1,double *vetor_local2){
+   int Passou_rank0= 0;
    int i;
-   vetor1 = malloc(*tamanho * (sizeof(double)));
-   vetor2 = malloc(*tamanho * (sizeof(double)));
-   vetor_local1 = malloc(*local_n*sizeof(double));
-   vetor_local2 = malloc(*local_n*sizeof(double));
+ 
    if(rank==0){
       
          printf("Digite o primeiro vetor\n");
          for(i=0;i<*tamanho;i++){
             scanf("%lf",&vetor1[i]);
          }
+         MPI_Scatter(vetor1,*local_n, MPI_DOUBLE, 
+               vetor_local1,*local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
          printf("Digite o segundo vetor\n");
          for(i=0;i<*tamanho;i++){ 
             scanf("%lf",&vetor2[i]);
          }
-          
-         MPI_Scatter(vetor1,*local_n, MPI_DOUBLE, 
-               vetor_local1,*local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
          MPI_Scatter(vetor2,*local_n, MPI_DOUBLE, 
                vetor_local2,*local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-       // free(vetor1);
-        //free(vetor2);
-         
+        free(vetor1);
+        free(vetor2);
+         Passou_rank0= 1;
    }else {
+   	  //if(Passou_rank0){
+   	  	//local_n=2;
+   		
         MPI_Scatter(vetor1,*local_n, MPI_DOUBLE, 
                vetor_local1,*local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
          MPI_Scatter(vetor2,*local_n, MPI_DOUBLE, 
                vetor_local2,*local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-   }
+   
+   	 // }
+   	}
 }
 
-void Soma_locais(int* local_n,double *vetor_local1,int *escalar, double *vetor_local2,double *soma_local){
+void Soma_locais(int* local_n,double *vetor_local1,int *escalar, double *vetor_local2,double *soma_local,double *resultado,int rank){
    int i;
    soma_local = malloc(sizeof(double));
+   resultado= malloc(sizeof(double));
    // calculando as somas locais
-   printf("\n entra aqui\n" );
+   printf("\n vetor 1 \n" );
    for(i=0;i<*local_n;i++){
-      vetor_local1[i] *= *escalar;
-      vetor_local1[i] *= vetor_local2[i];
-      *soma_local += vetor_local1[i];
+      printf("%lf",vetor_local1[i]);
+     
+     // *soma_local = (*soma_local) + vetor_local1[i];
    
    }
+
+/*
+   for(i=0;i<*local_n;i++){
+      vetor_local1[i] = vetor_local1[i] *(*escalar);
+      vetor_local1[i] =vetor_local1[i]*vetor_local2[i];
+     // *soma_local = (*soma_local) + vetor_local1[i];
+   
+   }
+   //Adiciona as somas locais no resultado
+  // MPI_Reduce(soma_local, resultado, 1, MPI_DOUBLE, MPI_SUM, 0,
+    //     MPI_COMM_WORLD);
+
+   if(rank == 0){
+      printf("O resultado final: %lf\n",*resultado);
+      free(vetor_local1);
+      free(vetor_local2);
+   }*/
 }
 
