a2aa2476e172dbf59aa7c1e0fce403d4a88b14ca
alex7alves <alex7alves10@hotmail.com>
2017-10-26 20:59:33 -0300

    Árvore para tamanhos impares e não potencia de 2

--- Árvore impar/arvore_impar.c
+++ Árvore impar/arvore_impar.c
@@ -1,6 +1,7 @@
 
 /* Autor : Alex Alves
-   Programa para calculo soma em arvore com processos impares
+   Programa para calculo soma em arvore com processos impares 
+   e nao potencia de 2
 */
 #include <stdio.h>
 #include <string.h>  
@@ -17,16 +18,16 @@ int lg2(int n){
    return i;
 }
 
-
+// FAlTA ajustar o tamanho da profundidade da arvore para ser auto-ajustável
 int main(void) {
    int size; // quantidade maxima de processos
    int rank;  // numero do  processo atual
-   int valor[7],local,divisor=2,difference=1,soma; 
+   int valor[11],local,divisor=2,difference=1,soma; 
    MPI_Status stat; // guarda o status 
    int QuantidadeCoreExecutando,IndiceUltimoCoreExecucao,aux;
    // Valores iniciais para cada processo
-   valor[0]= 5,valor[1]=2,valor[2]=-1, valor[3]= -3 ;
-   valor[4]= 6 , valor[5]= 5, valor[6]= -7;//valor[7]=2,valor[8]=1,valor[9]=3;// valor[7]=2;
+   valor[0]= 5,valor[1]=2,valor[2]=-1, valor[3]= -3;
+   valor[4]= 6 , valor[5]= 5, valor[6]= -7,valor[7]=2,valor[8]=1,valor[9]=3,valor[10]=2;
    // tamanho do vetor
    int n = sizeof(valor)/sizeof(valor[0]);
    // Inicia o MPI
@@ -40,19 +41,20 @@ int main(void) {
    QuantidadeCoreExecutando = size;
    IndiceUltimoCoreExecucao=size-1;
    
-   for(int j=1;j<=lg;j++){
+   for(int j=1;j<=4;j++){
  //  while( divisor<= size) {
       for(int IndiceCore=0;IndiceCore<size ;IndiceCore= IndiceCore+divisor) {  
-         
-         if(rank==(IndiceCore+difference)){  
+         //(IndiceCore+difference)<size pra nao dar erro de enviar para numero inxistente
+         //tipo ta no rank 8 e envia pro 16 mas no maximo eh 14
+         if(rank==(IndiceCore+difference) && (IndiceCore+difference)<size){  
             MPI_Send(&valor[IndiceCore+difference],1,MPI_INT,IndiceCore,0,MPI_COMM_WORLD);    
-            printf(" Sou o processo  %d enviei pro %d\n ",rank, IndiceCore);
+            printf(" Sou o processo  %d enviei pro %d\n",rank, IndiceCore);
          }else if(QuantidadeCoreExecutando%2!=0 && IndiceCore==IndiceUltimoCoreExecucao){
             // se for impar manda o ultimo processo para a proxima 
             // linha da arvore
             MPI_Send(&valor[IndiceUltimoCoreExecucao],1,MPI_INT,IndiceUltimoCoreExecucao,0,MPI_COMM_WORLD);
             
-         }else if(rank==IndiceCore) {
+         }else if(rank==IndiceCore &&  (IndiceCore+difference)<size) {
             // rank pares
             //I_Reduce(&valor[IndiceCore],&soma,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD); 
             MPI_Recv(&local,1,MPI_INT,IndiceCore+difference,0,MPI_COMM_WORLD,&stat);
@@ -73,7 +75,7 @@ int main(void) {
       IndiceUltimoCoreExecucao =IndiceUltimoCoreExecucao-aux;
    }
    if(rank==0){
-   	   printf("O processo %d terminou com o valor %d\n",rank,valor[0 ]); 
+   	   printf("O processo %d terminou com o valor %d\n",rank,valor[0]); 
    }
    MPI_Finalize(); 
    return 0;
