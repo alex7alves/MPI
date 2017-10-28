3eb535e63484b341915d55da7e9982ecb356d3e8
alex7alves <alex7alves10@hotmail.com>
2017-10-28 10:30:26 -0300

    Finalizando e tirando prints desnecessários

--- Árvore completa/arvore_completa.c
+++ Árvore completa/arvore_completa.c
@@ -1,7 +1,10 @@
 
 /* Autor : Alex Alves
-   Programa para calculo soma em arvore com processos impares 
-   e nao potencia de 2
+   Programa para calculo soma em arvore com qualquer numero de processos 
+   e qualquer tamanho do vetor
+
+   execute assim :
+   mpiexec -n p ./arvore_completa 1 2 3 4 5 6 7 ....
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,7 +30,6 @@ int SomaLocal(int *vetor,int a,int b){
    return soma;
 }
 
-// FAlTA ajustar o tamanho da profundidade da arvore para ser auto-ajustável
 int main(int argc, char *argv[]) {
    int size; // quantidade maxima de processos
    int rank;  // numero do  processo atual
@@ -36,7 +38,7 @@ int main(int argc, char *argv[]) {
    int local_n, local_a, local_b;
    MPI_Status stat; // guarda o status 
    int QuantidadeCoreExecutando,IndiceUltimoCoreExecucao,aux;
-   
+   // pegando vetor em inteiro
    for(int i=0;i<argc-1;i++){
       vetor[i]=atoi(argv[i+1]);
    }
@@ -51,13 +53,14 @@ int main(int argc, char *argv[]) {
    QuantidadeCoreExecutando = size;
    IndiceUltimoCoreExecucao=size-1;
    local_n=(n/size);
-   if (n%size==0)  // caso o número total de elementos do vetor seja divisível pela qtd de núcleos
+   //Numero  de elementos do vetor  divisível pela quantidade de processos
+   if (n%size==0)  
    {
       local_a=rank*(n/size);
       local_b=local_a+(n/size)-1;
       valor[rank]=SomaLocal(vetor,local_a,local_b);
    }
-   else  //caso o número total de elementos do vetor NÃO seja divisível pela qtd de núcleos 
+   else // Tamanho do vetor nao divisel pelo numero de processos
    {
       if(rank<(n%size))
       {
@@ -75,14 +78,12 @@ int main(int argc, char *argv[]) {
         valor[rank]=SomaLocal(vetor,local_a,local_b); 
       }
    }
-   for(int j=1;j<=4;j++){
- //  while( divisor<= size) {
+   for(int j=1;j<=lg;j++){
       for(int IndiceCore=0;IndiceCore<size ;IndiceCore= IndiceCore+divisor) {  
          //(IndiceCore+difference)<size pra nao dar erro de enviar para numero inxistente
          //tipo ta no rank 8 e envia pro 16 mas no maximo eh 14
          if(rank==(IndiceCore+difference) && (IndiceCore+difference)<size){  
             MPI_Send(&valor[IndiceCore+difference],1,MPI_INT,IndiceCore,0,MPI_COMM_WORLD);    
-            printf(" Sou o processo  %d enviei pro %d\n",rank, IndiceCore);
          }else if(QuantidadeCoreExecutando%2!=0 && IndiceCore==IndiceUltimoCoreExecucao){
             // se for impar manda o ultimo processo para a proxima 
             // linha da arvore
@@ -90,11 +91,8 @@ int main(int argc, char *argv[]) {
             
          }else if(rank==IndiceCore &&  (IndiceCore+difference)<size) {
             // rank pares
-            //I_Reduce(&valor[IndiceCore],&soma,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD); 
             MPI_Recv(&local,1,MPI_INT,IndiceCore+difference,0,MPI_COMM_WORLD,&stat);
             valor[IndiceCore]=valor[IndiceCore]+local;
-            printf(" Sou o processo %d recebi o valor  %d apos a soma ficou %d\n",rank,local, valor[IndiceCore]);
-         
          }else if(QuantidadeCoreExecutando%2!=0 && IndiceCore==IndiceUltimoCoreExecucao){
             // se for impar equilibra a arvore 
             MPI_Recv(&local,1,MPI_INT,IndiceUltimoCoreExecucao,0,MPI_COMM_WORLD,&stat);
@@ -107,6 +105,9 @@ int main(int argc, char *argv[]) {
       difference = difference *2;
       QuantidadeCoreExecutando=QuantidadeCoreExecutando/2;
       IndiceUltimoCoreExecucao =IndiceUltimoCoreExecucao-aux;
+      if(QuantidadeCoreExecutando>1 && j==lg){
+         lg=lg+1;
+      }
    }
    if(rank==0){
          printf("O processo %d terminou com o valor %d\n",rank,valor[0]); 
